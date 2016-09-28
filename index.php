39c8e9890987466208d8570082549c200339b9e3
Sandro Comes Zacares <sacoza@floridauniversitaria.es>
2016-09-28 11:32:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+//fichero index.php
+//creado por Paco Gomez
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+        echo "Sandro";
+        echo "Comes";
+        echo "Zacares";
+      ?>
+  </head>
+  <body>
+</html>
